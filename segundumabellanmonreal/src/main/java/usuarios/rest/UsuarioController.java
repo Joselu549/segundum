120ae9874edfd1b45445aef4e9325dfe8fe1f65c
José Luis Abellán Monreal <joseluisabellanmonreal@gmail.com>
2026-02-20 22:16:15 +0100

    Add update user by ID

--- segundumabellanmonreal/src/main/java/usuarios/rest/UsuarioController.java
+++ segundumabellanmonreal/src/main/java/usuarios/rest/UsuarioController.java
@@ -2,7 +2,9 @@ package usuarios.rest;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,34 +18,67 @@ import usuarios.servicio.IServicioUsuarios;
 
 @Path("users")
 public class UsuarioController {
-  private final IServicioUsuarios servicioUsuarios = FactoriaServicios.getServicio(IServicioUsuarios.class);
+    private final IServicioUsuarios servicioUsuarios = FactoriaServicios.getServicio(IServicioUsuarios.class);
 
-  @Context
-  private UriInfo uriInfo;
+    @Context
+    private UriInfo uriInfo;
 
-  @POST
-  @Consumes({ MediaType.APPLICATION_JSON })
-  public Response registrarUsuario(UsuarioDTO usuario) throws Exception {
-    try {
-      LocalDate fechaNacimiento = LocalDate.parse(usuario.getFechaNacimiento());
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON })
+    public Response registrarUsuario(UsuarioDTO usuario) throws Exception {
+        try {
+            LocalDate fechaNacimiento = LocalDate.parse(usuario.getFechaNacimiento());
 
-      String id = servicioUsuarios.registrarUsuario(
-          usuario.getEmail(),
-          usuario.getNombre(),
-          usuario.getApellidos(),
-          usuario.getTelefono(),
-          "No hay dirección",
-          fechaNacimiento,
-          usuario.getPassword());
-      return Response.created(this.uriInfo.getAbsolutePathBuilder().path(id).build()).build();
-    } catch (IllegalArgumentException e) {
-      return Response.status(Response.Status.BAD_REQUEST)
-          .entity("Error al registrar el usuario: " + e.getMessage())
-          .build();
-    } catch (Exception e) {
-      return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-          .entity("Error al registrar el usuario: " + e.getMessage())
-          .build();
+            String id = servicioUsuarios.registrarUsuario(
+                    usuario.getEmail(),
+                    usuario.getNombre(),
+                    usuario.getApellidos(),
+                    usuario.getTelefono(),
+                    "No hay dirección",
+                    fechaNacimiento,
+                    usuario.getPassword());
+            return Response.created(this.uriInfo.getAbsolutePathBuilder().path(id).build()).build();
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Error al registrar el usuario: " + e.getMessage())
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error al registrar el usuario: " + e.getMessage())
+                    .build();
+        }
     }
-  }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    public Response modificarUsuario(@PathParam("id") String id, UsuarioDTO usuario) {
+        try {
+            if (usuario.getNombre() != null) {
+                servicioUsuarios.modificarNombre(id, usuario.getNombre());
+            }
+            if (usuario.getApellidos() != null) {
+                servicioUsuarios.modificarApellidos(id, usuario.getApellidos());
+            }
+            if (usuario.getPassword() != null) {
+                servicioUsuarios.modificarPassword(id, usuario.getPassword());
+            }
+            if (usuario.getFechaNacimiento() != null) {
+                LocalDate fechaNacimiento = LocalDate.parse(usuario.getFechaNacimiento());
+                servicioUsuarios.modificarFechaNacimiento(id, fechaNacimiento);
+            }
+            if (usuario.getTelefono() != null) {
+                servicioUsuarios.modificarTelefono(id, usuario.getTelefono());
+            }
+            return Response.noContent().build();
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Error al modificar el usuario: " + e.getMessage())
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error al modificar el usuario: " + e.getMessage())
+                    .build();
+        }
+    }
 }
