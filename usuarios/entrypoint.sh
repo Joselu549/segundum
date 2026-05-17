8cada6e38a0f563a8d1ee2c6231ba697442a4340
José Luis Abellán Monreal <joseluisabellanmonreal@gmail.com>
2026-05-17 20:30:48 +0200

    [FINAL] Terminado ArSo

--- usuarios/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-JDBC_URL="jdbc:mysql://${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DB}?serverTimezone=CET&useSSL=false&allowPublicKeyRetrieval=true"
-export JDK_JAVA_OPTIONS="${JDK_JAVA_OPTIONS} -Djavax.persistence.jdbc.url=${JDBC_URL} -Djavax.persistence.jdbc.user=${MYSQL_USER} -Djavax.persistence.jdbc.password=${MYSQL_PASSWORD}"
-exec "$@"
